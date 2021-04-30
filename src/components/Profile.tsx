3aea7a872a19d616517cbe84d0c01809b768e684
r0cc4t0 <guilherme.roccato@outlook.com>
2021-04-29 21:54:32 -0300

    Profile and Completed Challenges Components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css';
+
+function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+      <div>
+        <strong>Diego Fernandes</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
