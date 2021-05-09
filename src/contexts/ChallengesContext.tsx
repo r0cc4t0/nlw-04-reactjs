3f048d8aa1a0f84e700829f234192382df592319
r0cc4t0 <guilherme.roccato@outlook.com>
2021-05-08 22:55:04 -0300

    Notification Sound

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -44,6 +44,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
     const challenge = challenges[randomChallengeIndex];
     setActiveChallenge(challenge);
+    new Audio('/notification.mp3').play();
     if (Notification.permission === 'granted') {
       new Notification('Novo Desafio 🎉', {
         body: `Valendo ${challenge.amount} XP!`
